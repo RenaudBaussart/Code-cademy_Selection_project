0bc9784267973f4e67e20bda2f5c64db9b5ac36f
Renaud Baussart <Renaud.Baussart@proton.me>
2025-09-08 14:07:28 +0200

    feat: set up dropdown panel animation

--- src/chat-room_project/src/app/pages/chat-room/chat-room.component.ts
+++ src/chat-room_project/src/app/pages/chat-room/chat-room.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { ChatMessage } from '../../model/chat-message';
 import { CommonModule } from '@angular/common';
+import { ConnectionDropdownPanelComponent } from '../connection-dropdown-panel/connection-dropdown-panel.component';
 
 @Component({
   selector: 'app-chat-room',
-  imports: [CommonModule],
+  imports: [CommonModule,ConnectionDropdownPanelComponent],
   templateUrl: './chat-room.component.html',
   styleUrl: './chat-room.component.scss'
 })
 export class ChatRoomComponent {
   backgroundUrl ='site_background.jpg';
+  isDropdownOpen:boolean = false;
 
+  public toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+    console.log(this.isDropdownOpen + '')
+  }
   //to make test
   chatMessages:Array<ChatMessage> = [ new ChatMessage(1, 'Alice', 'Bonjour à tous !'),
     new ChatMessage(2, 'Bob', 'Salut Alice !'),
